c9c8f3c20f3d2b35c5a4f35914cc6130ab08edb4
Amaury DAVID <amaury.david@outlook.com>
2020-12-14 17:11:01 +0100

    Fixed another crash.

--- Sources/VisualEffectView.swift
+++ Sources/VisualEffectView.swift
@@ -113,7 +113,7 @@ private extension VisualEffectView {
     }
     
     /// Sets the value for the key on the blurEffect.
-    func _setValue<T>(_ value: T, forKey key: Key) {
+    func _setValue<T>(_ value: T?, forKey key: Key) {
         blurEffect.setValue(value, forKeyPath: key.rawValue)
         if #available(iOS 14, *) {} else {
             self.effect = blurEffect
