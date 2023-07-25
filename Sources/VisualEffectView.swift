72974f7320e95f684f8f13d1a84650aef768192b
Lasha Efremidze <efremidzel@hotmail.com>
2023-07-25 01:34:43 -0700

    Update VisualEffectView.swift

--- Sources/VisualEffectView.swift
+++ Sources/VisualEffectView.swift
@@ -115,7 +115,7 @@ private extension VisualEffectView {
     /// Sets the value for the key on the blurEffect.
     func _setValue<T>(_ value: T?, forKey key: Key) {
         blurEffect.setValue(value, forKeyPath: key.rawValue)
-        if #available(iOS 14, *) {} else {
+        if #unavailable(iOS 14) {
             self.effect = blurEffect
         }
     }
