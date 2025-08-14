157c63c71fb6ced13010bd6121865a4adb1ec0d9
Lasha Efremidze <efremidzel@hotmail.com>
2025-08-14 02:49:42 -0700

    Create ExampleApp.swift

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Lasha Efremidze on 8/14/25.
+//  Copyright © 2025 Lasha Efremidze. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
