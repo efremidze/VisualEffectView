3bbee2ec9c6f632ccfc8f5f5c4b0c5ede305501d
Lasha Efremidze <efremidzel@hotmail.com>
2025-08-14 03:17:37 -0600

    Update Package.swift (#52)

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "VisualEffectView",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v14),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
@@ -21,10 +22,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "VisualEffectView",
-            dependencies: [],
-            path: "Sources"
-        )
+        .target(name: "VisualEffectView")
     ]
 )
