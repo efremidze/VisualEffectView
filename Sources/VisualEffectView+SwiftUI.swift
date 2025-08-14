2cd639ca3ffe7ec37d63ddb94aaad71d9701985a
Lasha Efremidze <efremidzel@hotmail.com>
2025-08-14 11:24:42 -0700

    Update VisualEffectView+SwiftUI.swift

--- Sources/VisualEffectView+SwiftUI.swift
+++ Sources/VisualEffectView+SwiftUI.swift
@@ -1,5 +1,5 @@
 //
-//  VisualEffectView_SwiftUI.swift
+//  VisualEffectView+SwiftUI.swift
 //  VisualEffectView
 //
 //  Created by 朱浩宇 on 2023/5/10.
@@ -8,12 +8,51 @@
 
 import SwiftUI
 
+/**
+ A SwiftUI view that applies a visual effect to the background of its content.
+ 
+ This view uses the `VisualEffectView` class to create a blur effect on the background of its content.
+ 
+ The effect can be customized with parameters such as tint color, tint alpha, blur radius, and scale.
+ */
 public struct VisualEffect: UIViewRepresentable {
+    /**
+     The tint color to apply to the blur effect.
+     
+     The default value is `nil`.
+     */
     let colorTint: Color?
+
+    /**
+     The alpha component of the tint color.
+     
+     The default value is `0.0`.
+     */
     let colorTintAlpha: CGFloat
+
+    /**
+     The radius of the blur effect.
+     
+     The default value is `0.0`.
+     */
     let blurRadius: CGFloat
+
+    /**
+     The scale factor for the blur effect.
+     
+     The default value is `1.0`.
+     */
     let scale: CGFloat
     
+    /**
+     Initializes a `VisualEffect` view with the specified parameters.
+     
+     - Parameters:
+        - colorTint: The tint color to apply to the blur effect. Defaults to `nil`.
+        - colorTintAlpha: The alpha component of the tint color. Defaults to `0.0`.
+        - blurRadius: The radius of the blur effect. Defaults to `0.0`.
+        - scale: The scale factor for the blur effect. Defaults to `1.0`.
+     */
     public init(colorTint: Color? = nil, colorTintAlpha: CGFloat = 0, blurRadius: CGFloat = 0, scale: CGFloat = 1) {
         self.colorTint = colorTint
         self.colorTintAlpha = colorTintAlpha
@@ -45,16 +84,14 @@ public struct VisualEffect: UIViewRepresentable {
     }
 }
 
-struct VisualEffect_Previews: PreviewProvider {
-    static var previews: some View {
-        ZStack {
-            Color.blue
-                .frame(width: 400, height: 400)
-            Color.red
-                .frame(width: 200, height: 100)
-            VisualEffect(colorTint: .white, colorTintAlpha: 0.5, blurRadius: 18)
-                .frame(width: 300, height: 200)
-        }
+#Preview {
+    ZStack {
+        Color.blue
+            .frame(width: 400, height: 400)
+        Color.red
+            .frame(width: 200, height: 100)
+        VisualEffect(colorTint: .white, colorTintAlpha: 0.5, blurRadius: 18)
+            .frame(width: 300, height: 200)
     }
 }
 
