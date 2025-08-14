65b2335ef2e21ecc0c4919d86f210f0355c34ced
Lasha Efremidze <efremidzel@hotmail.com>
2025-08-14 02:33:41 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,5 +22,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "VisualEffectView")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
