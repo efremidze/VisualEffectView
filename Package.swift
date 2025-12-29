29a6630fa0769ac4c7d0e9f54146ee5272a2ef5f
Lasha Efremidze <efremidzel@hotmail.com>
2025-12-28 18:16:25 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,5 +23,5 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "VisualEffectView")
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v5, .version("6")]
 )
